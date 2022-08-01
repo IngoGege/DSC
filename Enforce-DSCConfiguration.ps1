1c8d6642bfb3c2139bc975aefe61ff6a9b9f6089
IngoGege <ingo.gegenwarth@sap.com>
2022-08-01 16:19:47 +0200

    Initial upload

---
+++ Enforce-DSCConfiguration.ps1
@@ -0,0 +1,332 @@
+﻿<#
+
+.SYNOPSIS
+
+Created by: https://ingogegenwarth.wordpress.com/
+Version:    42 ("What do you get if you multiply six by nine?")
+Changed:    14.09.2014
+
+.LINK
+http://www.get-blog.com/?p=189
+http://learn-powershell.net/2012/05/13/using-background-runspaces-instead-of-psjobs-for-better-performance/
+https://msdn.microsoft.com/powershell/dsc/troubleshooting
+
+.DESCRIPTION
+
+This script allows you to trigger a consistency check on target nodes. Besides this you can stop the WmiPrvSE process and clear DSC related caches. The script is able to run multithreaded.
+
+.PARAMETER Computername
+
+ A single computer or an array of computernames. This parameter supports pipeline.
+
+.PARAMETER Credential
+
+Credential to be used to connect to the remote computer.
+
+.PARAMETER KillWmiPrvSE
+
+This will try to stop the WmiPrvSE process, which is responsible for the DSC engine, before it triggers the consistency check.
+
+.PARAMETER MultiThread
+
+By default multiple computers will be processed sequential. Use this switch to process all computers in parallel.
+
+.PARAMETER OperationTimeoutSec
+
+The time until the command Invoke-CimMethod runs into a timeout against the remote computer.
+
+.PARAMETER Threads
+
+ If MultiThread is used this defines the maximum number of parallel threads.
+
+.PARAMETER MaxResultTime
+
+If MultiThread is used this defines the time a check must complete. Default is 240.
+
+.EXAMPLE 
+
+Enforce the consistency check against multiple computers
+.\Enforce-DSCConfiguration.ps1 -Computername fabex01,fabex02 -Verbose
+
+Enforce the consistency check against multiple computers, stop the WmiPrvSE process, clear the cache and run the script mutithreaded
+.\Enforce-DSCConfiguration.ps1 -Computername fabex01,fabex02 -Verbose -MultiThread -KillWmiPrvSE -ClearCache
+
+Pipeline objects into the script
+Get-ADComputer -Filter { msExchCapabilityIdentifiers -eq '1'} | select DNSHostName | .\Enforce-DSCConfiguration.ps1 -Verbose -MultiThread
+#>
+
+[cmdletbinding()]
+param (
+    [Parameter( Mandatory=$True, ValueFromPipeline=$True, ValueFromPipelinebyPropertyName=$True, Position=0)]
+    [Alias('Fqdn','DNSHostName','ServerName','Name','Computer')]
+    [string[]]$Computername,
+    
+    [Parameter( Mandatory=$False, Position=1)]
+    [System.Management.Automation.PSCredential]$Credential,
+    
+    [parameter( Mandatory=$false, Position=2)]
+    [switch]$KillWmiPrvSE,
+
+    [parameter( Mandatory=$false, Position=3)]
+    [switch]$ClearCache,
+    
+    [parameter( Mandatory=$false, Position=4)]
+    [switch]$MultiThread,
+    
+    [Parameter( Mandatory=$False, Position=5)]
+    [int]$OperationTimeoutSec = 0,
+    
+    [parameter( Mandatory=$false, Position=6)]
+    [ValidateRange(0,20)]
+    [int]$Threads= '15',
+    
+    [parameter( Mandatory=$false, Position=7)]
+    [int]$MaxResultTime='240'
+)
+Begin {
+$Results = @()
+
+#initiate runspace and make sure we are using single-threaded apartment STA
+$Jobs = @()
+$Sessionstate = [system.management.automation.runspaces.initialsessionstate]::CreateDefault()
+$RunspacePool = [RunspaceFactory]::CreateRunspacePool(1, $Threads,$Sessionstate, $Host)
+$RunspacePool.ApartmentState = "STA"
+$RunspacePool.Open()
+[int]$j='1'
+
+function EnforceDSCConfiguration {
+[cmdletbinding()]
+param (
+    [Parameter(Mandatory=$True, Position=0)]
+    [Alias('Fqdn','DNSHostName','ServerName','Name','Computer')]
+    [string[]]$Computername,
+    
+    [Parameter(Mandatory=$False, Position=1)]
+    [System.Management.Automation.PSCredential]$Credential,
+    
+    [Parameter(Mandatory=$False, Position=2)]
+    [Int]$OperationTimeoutSec = 0,
+
+    [switch]$KillWmiPrvSE,
+
+    [switch]$ClearCache
+    )
+function KillWmiPrvSE {
+[cmdletbinding()]
+param (
+    [string[]]$Computername,
+    [System.Management.Automation.PSCredential]$Credential,
+    [Int]$OperationTimeoutSec = 0
+)
+#$ScriptBlock = {Get-Process -Name WmiPrvSE | Stop-Process -Confirm:$false -Force | Out-Null}
+$ScriptBlock = {
+    $ProcID= Get-WmiObject msft_providers | Where-Object {$_.provider -like 'dsccore'} | Select-Object -ExpandProperty HostProcessIdentifier
+    If ($ProcID) {
+        Get-Process -ID $ProcID| Stop-Process -Confirm:$false -Force | Out-Null
+    }
+    }
+
+ForEach ($Computer in $Computername) {
+Write-Verbose "Will terminate all WmiPrvSE processes on computer:$($Computer)"
+    If ($Credential -eq $null) {
+        $currentuser = $Env:USERNAME
+        Write-Verbose "No credential given. Using logged on user: $($currentuser)"
+        try {
+            Invoke-Command -ScriptBlock $ScriptBlock -ComputerName $Computer -ErrorAction Stop
+        }
+        catch {
+            Write-Warning $_.Exception.Message
+        }
+    }
+    Else {
+        Write-Verbose "Using given credentials $($Credential.Username)"
+        try {
+            Invoke-Command -ScriptBlock $ScriptBlock -ComputerName $Computer -Credential $Credential -ErrorAction Stop
+        }
+        catch {
+            Write-Warning $_.Exception.Message
+        }
+    }
+}
+}
+
+function ClearDSCFileCache {
+[cmdletbinding()]
+param (
+    [string[]]$Computername,
+    [System.Management.Automation.PSCredential]$Credential
+)
+$ScriptBlock = {Get-ChildItem $env:Windir\System32\Configuration\BuiltinProvCache -Filter *.cache -Recurse | Remove-Item -Force}
+ForEach ($Computer in $Computername) {
+Write-Verbose "Will delete all caches of DSC on computer:$($Computer)"
+    If ($Credential -eq $null) {
+        $currentuser = $Env:USERNAME
+        Write-Verbose "No credential given. Using logged on user: $($currentuser)"
+        try {
+            Invoke-Command -ScriptBlock $ScriptBlock -ComputerName $Computer -ErrorAction Stop
+        }
+        catch {
+            Write-Warning $_.Exception.Message
+        }
+    }
+    Else {
+        Write-Verbose "Using given credentials $($Credential.Username)"
+        try {
+            Invoke-Command -ScriptBlock $ScriptBlock -ComputerName $Computer -Credential $Credential -ErrorAction Stop
+        }
+        catch {
+            Write-Warning $_.Exception.Message
+        }
+    }
+}
+}
+
+If ($KillWmiPrvSE -eq $true) {
+    KillWMIPrvSE -Computername $Computername -Credential $Credential
+}
+
+If ($ClearCache -eq $true) {
+    ClearDSCFileCache -Computername $Computername -Credential $Credential
+}
+
+#create hashtable of parameters
+$params = @{
+Namespace = 'root/Microsoft/Windows/DesiredStateConfiguration'
+ClassName = 'MSFT_DSCLocalConfigurationManager'
+MethodName = 'PerformRequiredConfigurationChecks'
+Arguments = @{  Flags = [uint32] 1  } }
+
+ForEach ($Computer in $Computername) {
+Write-Verbose "Working on computer:$($Computer)"
+    If ($Credential -eq $null) {
+        $currentuser = $Env:USERNAME
+        Write-Verbose "No credential given. Using logged on user: $($currentuser)"
+        try {
+            $Session = New-CimSession -ComputerName $Computer -ErrorAction Stop
+        }
+        catch {
+            Write-Warning $_.Exception.Message
+        }
+    }
+    Else {
+        Write-Verbose "Using given credentials $($Credential.Username)"
+        try {
+            $Session = New-CimSession -ComputerName $Computer -Credential $Credential -ErrorAction Stop
+        }
+        catch {
+            Write-Warning $_.Exception.Message
+        }
+    }
+    try {
+        Invoke-CimMethod @params -CimSession $Session -ErrorAction Stop -OutVariable result -OperationTimeoutSec $OperationTimeoutSec
+    }
+    catch {
+        Write-Warning $_.Exception.Message
+    }
+}
+}
+
+}
+
+Process {
+
+#if multi-threaded create jobs
+If ($MultiThread) {
+    #create scriptblock from function
+    $ScriptBlock = [scriptblock]::Create((Get-ChildItem Function:\EnforceDSCConfiguration).Definition)
+    ForEach($Computer in $Computername) {
+        try{
+            $PowershellThread = [powershell]::Create() #.AddScript($ScriptBlock).AddParameter('Computername',$Computer)
+            If ($PSBoundParameters.ContainsKey('Verbose')){
+                #Write-Verbose "MultiThreads Verbose exists!"
+                $PowershellThread.AddScript({$VerbosePreference = 'Continue'}) | Out-Null
+            }
+            $PowershellThread.AddScript($ScriptBlock).AddParameter('Computername',$Computer) | Out-Null
+            If ($Credential) {
+                $PowershellThread.AddParameter('Credential',$Credential) | Out-Null
+            }
+            If ($KillWmiPrvSE) {
+                $PowershellThread.AddParameter('KillWmiPrvSE',$KillWmiPrvSE) | Out-Null
+            }
+            If ($ClearCache) {
+                $PowershellThread.AddParameter('ClearCache',$ClearCache) | Out-Null
+            }
+            $PowershellThread.AddParameter('OperationTimeoutSec',$OperationTimeoutSec) | Out-Null
+            $PowershellThread.RunspacePool = $RunspacePool
+            $Handle = $PowershellThread.BeginInvoke()
+            $Job = "" | Select-Object Handle, Thread, object
+            $Job.Handle = $Handle
+            $Job.Thread = $PowershellThread
+            $Job.Object = $Computer
+            $Jobs += $Job
+        }
+        catch {
+            #$Error[0].Exception
+            $_.Exception.Message
+        }
+    }
+}
+Else {
+    $Results += EnforceDSCConfiguration -Computername $Computername -Credential $Credential -OperationTimeoutSec $OperationTimeoutSec -KillWmiPrvSE:$KillWmiPrvSE -ClearCache:$ClearCache
+}
+
+}
+
+End {
+#monitor and retrieve the created jobs
+If ($MultiThread) {
+    $SleepTimer = 200
+    $ResultTimer = Get-Date
+    While (@($Jobs | Where-Object {$_.Handle -ne $Null}).count -gt 0)  {
+    $Remaining = "$($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False}).object)"
+        If ($Remaining.Length -gt 60){
+            $Remaining = $Remaining.Substring(0,60) + "..."
+        }
+        Write-Progress `
+            -Activity "Waiting for Jobs - $($Threads - $($RunspacePool.GetAvailableRunspaces())) of $Threads threads running" `
+            -PercentComplete (($Jobs.count - $($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False}).count)) / $Jobs.Count * 100) `
+            -Status "$(@($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False})).count) remaining - $Remaining"
+
+        ForEach ($Job in $($Jobs | Where-Object {$_.Handle.IsCompleted -eq $True})){
+            Write-Progress `
+            -Activity "Waiting for Jobs - $($Threads - $($RunspacePool.GetAvailableRunspaces())) of $Threads threads running" `
+            -Status "Ready" `
+            -Completed
+            $Results += $Job.Thread.EndInvoke($Job.Handle)
+            $Job.Thread.Dispose()
+            $Job.Thread = $Null
+            $Job.Handle = $Null
+            $ResultTimer = Get-Date
+        }
+        If (($(Get-Date) - $ResultTimer).totalseconds -gt $MaxResultTime){
+            Write-Warning "Child script appears to be frozen for $($Job.Object), try increasing MaxResultTime"
+            #Exit
+        }
+        Start-Sleep -Milliseconds $SleepTimer
+    # kill all incomplete threads when hit "CTRL+q"
+    If ($Host.UI.RawUI.KeyAvailable) {
+        $KeyInput = $Host.UI.RawUI.ReadKey("IncludeKeyUp,NoEcho")
+        If (($KeyInput.ControlKeyState -cmatch '(Right|Left)CtrlPressed') -and ($KeyInput.VirtualKeyCode -eq '81')) {
+            Write-Host -fore red "Kill all incomplete threads....."
+                ForEach ($Job in $($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False})){
+                    Write-Host -fore yellow "Stopping job $($Job.Object) ...."
+                    $Job.Thread.Stop()
+                    $Job.Thread.Dispose()
+                    Write-Progress `
+                        -id 1 `
+                        -Activity "Waiting for Jobs - $($Threads - $($RunspacePool.GetAvailableRunspaces())) of $Threads threads running" `
+                        -PercentComplete (($Jobs.count - $($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False}).count)) / $Jobs.Count * 100) `
+                        -Status "$(@($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False})).count) remaining - $Remaining"
+                }
+            Write-Host -fore red "Exit script now!"
+            Exit
+        }
+    }
+    }
+    # clean-up
+    $RunspacePool.Close() | Out-Null
+    $RunspacePool.Dispose() | Out-Null
+    [System.GC]::Collect()
+}
+$Results | select @{l="Computer";e={$_.PSComputerName}},@{l="Result";e={$_.ReturnValue}}
+}
---
+++ New-DSCConfigsFromFiles.ps1
@@ -0,0 +1,319 @@
+﻿<#
+
+.SYNOPSIS
+
+Created by: https://ingogegenwarth.wordpress.com/
+Version:    42 ("What do you get if you multiply six by nine?")
+Changed:    14.09.2016
+
+.LINK
+
+.DESCRIPTION
+
+This script will create MOF files and publish them to a pull server out of given Configuration, Environment Data and a custom CSV file, which must contain a column called ServerName
+
+.PARAMETER ServerCSVFile
+
+Full path to the CSV file containing the nodes
+
+.PARAMETER DSCConfig
+
+Path to your DSC Configuration file
+
+.PARAMETER DSCConfigSettings
+
+Path to your DSC Environment Data file
+
+.PARAMETER MOFsPath
+
+Path to a folder to store the generated MOF files
+
+.PARAMETER CertPath
+
+Path to a folder where all the certificates will be stored
+
+.PARAMETER MOFsTargetPath
+
+Path to the folder on the pull server to publish the files. Default is 'C:\Program Files\WindowsPowerShell\DscService\Configuration'
+
+.PARAMETER PublishToPull
+
+Switch to publish the files to the folder defined in MOFsTargetPath. Default is $true
+
+.PARAMETER SkipPing
+
+By default the script uses Test-Connection to check the availability of a node. If you use this switch this test will be bypassed. Default is $false
+
+.PARAMETER SkipCert
+
+Switch to bypass the certificate export. This is useful when you are using the same certificate on all nodes. Note: If you are going not to encrypt your credentials, make sure you have set the property PSDscAllowPlainTextPassword to $true!
+
+.Parameter SkipCertVerification
+
+If set to $true the verification of the certificate will be skipped, which speed-up the process.
+
+.EXAMPLE 
+
+.\New-DSCConfigsFromFiles.ps1 -ServerCSVFile C:\DSC\CSVs\LabServers.csv -DSCConfig C:\DSC\Scripts\DemoConfig.ps1 -DSCConfigSettings C:\DSC\Scripts\DemoConfig-Config.psd1 -MOFsPath C:\DSC\Config -CertPath C:\DSC\Certs -PublishToPull -SkipPing -Verbose
+
+.NOTES
+#>
+
+[cmdletbinding()]
+param(
+    [ValidateScript({If (Test-Path $_ -PathType Leaf) {$True} Else {Throw "No file could not be found!"}})]
+    [parameter( Mandatory=$true, Position=0)]
+    [string]$ServerCSVFile,
+
+    [ValidateScript({If (Test-Path $_ -PathType Leaf) {$True} Else {Throw "No file could not be found!"}})]
+    [parameter( Mandatory=$false, Position=1)]
+    [string]$DSCConfig,
+
+    [ValidateScript({If (Test-Path $_ -PathType Leaf) {$True} Else {Throw "No file could not be found!"}})]
+    [parameter( Mandatory=$false, Position=2)]
+    [string]$DSCConfigSettings,
+    
+    [ValidateScript({If (Test-Path $_ -PathType Container) {$True} Else {Throw "Target folder for MOFs could not be found!"}})]
+    [parameter( Mandatory=$false, Position=3)]
+    [string]$MOFsPath = "C:\DSC\Config",
+
+    [parameter( Mandatory=$false, Position=4)]
+    [ValidateScript({If (Test-Path $_ -PathType Container) {$True} Else {Throw "Folder for certificates could not be found!"}})]
+    [string]$CertPath = "C:\DSC\Certs",
+
+    [parameter( Mandatory=$false, Position=5)]
+    [ValidateScript({If (Test-Path $_ -PathType Container) {$True} Else {Throw "Target folder to publish MOFs could not be found!"}})]
+    [string]$MOFsTargetPath = "C:\Program Files\WindowsPowerShell\DscService\Configuration",
+
+    [parameter( Mandatory=$false, Position=6)]
+    [switch]$PublishToPull = $true,
+
+    [parameter( Mandatory=$false, Position=7)]
+    [switch]$SkipPing=$true,
+
+    [parameter( Mandatory=$false, Position=8)]
+    [switch]$SkipCert,
+
+    [parameter( Mandatory=$false, Position=9)]
+    [boolean]$SkipCertVerification
+)
+
+Begin {
+
+function Get-EncryptionCertificate
+{
+    [CmdletBinding()]
+    param (
+        [string]$ComputerName,
+        [string]$Path,
+        [switch]$DoExport,
+        [boolean]$SkipCertVerify
+    )
+    If ($DoExport) {
+    [array]$returnValue= Invoke-Command -ComputerName $computerName -ScriptBlock {
+            if ($using:SkipCertVerify) {
+                $certificates = dir Cert:\LocalMachine\my | sort NOTAFTER -Descending |
+                                ?{$_.PrivateKey.KeyExchangeAlgorithm -and ($_.DnsNameList -match $env:ComputerName)} |
+                                select -First 1
+            }
+            else {
+                $certificates = dir Cert:\LocalMachine\my | sort NOTAFTER -Descending |
+                                ?{$_.PrivateKey.KeyExchangeAlgorithm -and $_.Verify() -and ($_.DnsNameList -match $env:ComputerName)} |
+                                select -First 1
+            }
+            $FW = (Get-NetFirewallRule -DisplayName "File and Printer Sharing (SMB-In)").Enabled
+            $Folder = $using:Path
+            $Server = $using:Computername
+            if ($certificates) {
+                # Create the folder to hold the exported public key
+                if (! (Test-Path $Folder)) {
+                    md $Folder | Out-Null
+                }
+                #Enable FW
+                If ($FW -eq "False") {
+                    #Write-Host "Enable FW rule!"
+                    Enable-NetFirewallRule -DisplayName "File and Printer Sharing (SMB-In)"
+                    #Sleep 2
+                }
+                # Export the public key to a well known location
+                $certPath = Export-Certificate -Cert $certificates -FilePath (Join-Path -path $Folder -childPath "$Server.cer") -Force
+                # Return the thumbprint, and exported certificate path
+                return @($certificates.Thumbprint,$certPath);
+            }
+        }
+    # Copy the exported certificate locally
+    $destinationPath = join-path -Path "$Path" -childPath "$Computername.cer"
+    Copy-Item -Path (join-path -path \\$Computername -childPath $returnValue[1].FullName.Replace(":","$"))  $destinationPath -Force | Out-Null
+    }
+    Else {
+    [array]$returnValue= Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+            if ($using:SkipCertVerify) {
+                $certificates = dir Cert:\LocalMachine\my | sort NOTAFTER -Descending |
+                                ?{$_.PrivateKey.KeyExchangeAlgorithm -and ($_.DnsNameList -match $env:ComputerName)} |
+                                select -First 1
+            }
+            else {
+                $certificates = dir Cert:\LocalMachine\my | sort NOTAFTER -Descending |
+                                ?{$_.PrivateKey.KeyExchangeAlgorithm -and $_.Verify() -and ($_.DnsNameList -match $env:ComputerName)} |
+                                select -First 1
+            }
+            if ($certificates) {
+                # Return the thumbprint
+                return $certificates.Thumbprint;
+            }
+        }   
+    }
+    # Return the thumbprint, and exported certificate path
+    return @($returnValue[0],$returnValue[1].FullName)
+}
+
+function PublishMOF
+{
+param (
+
+    [parameter( Mandatory=$true, Position=0)]
+    [string]$MOFFilePath,
+
+    [parameter( Mandatory=$true, Position=1)]
+    [string]$GUID,
+
+    [parameter( Mandatory=$true, Position=2)]
+    [string]$TargetFolder
+
+)
+$Destination = "$TargetFolder\$GUID.mof"
+Copy-Item -Path $MOFFilePath -Destination $Destination -Force
+New-DSCCheckSum -ConfigurationPath $Destination -OutPath $TargetFolder -Force
+}
+
+function ConvertTo-HashtableFromPsCustomObject
+{ 
+    param ( 
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [object[]]$psCustomObject 
+    ); 
+    process { 
+        foreach ($myPsObject in $psCustomObject) { 
+            $output = @{}; 
+            $myPsObject | Get-Member -MemberType *Property | % { 
+            $output.($_.name) = $myPsObject.($_.name); 
+            } 
+            $output; 
+        }
+    }
+}
+
+[string[]]$ComputerName
+Write-Verbose "Import CSV file..."
+$ComputerName = Import-Csv $ServerCSVFile | ?{$_.ServerName -notmatch 'Version'} #|Out-Null
+Write-Verbose "Import Config from $($DSCConfig) ..."
+#import Config
+$Config = (Get-Content $DSCConfig | Out-String)
+$Config = $Config.Substring('0',$Config.LastIndexOf('}')+1)
+#execute expression
+Write-Verbose "Invoke Config ..."
+Invoke-Expression $Config
+#get name of the Config to execute it later
+$ConfigName = (dir function:* | ?{$_.commandtype -eq 'Configuration'} | select -First 1).Name
+$ErrorActionPreference = "Stop"
+}
+
+Process {
+
+
+ForEach ($Computer in $ComputerName) {
+#create ConfigurationData
+If ($DSCConfigSettings) {
+    Write-Verbose "Create ConfigurationData from $($DSCConfigSettings) ..."
+    $ConfigurationData = Invoke-Expression (Get-Content $DSCConfigSettings | Out-String)
+}
+Else {
+    #create hashtable ConfigurationData
+    Write-Verbose "Create ConfigurationData from scratch ..."
+    $ConfigurationData = @{
+        AllNodes = @();
+        NonNodeData = ""
+    }
+}
+
+$Name = $Computer.servername
+Write-Host "Working on $($Name) ..."
+#check availability
+If ((!(Test-Connection -ComputerName $Name -Quiet -Count 1)) -and ($SkipPing -eq $false)) {
+    Write-Host -ForegroundColor Red "Could not reach $($Name)! Will skip this one!"
+    Continue
+}
+If (!($SkipCert -eq $true)) {
+#get certificate data
+try {
+    Write-Verbose "Trying to retrieve Cert from node ..."
+    $RemoteCert = Get-EncryptionCertificate -Computername $Name -DoExport -Path $CertPath -SkipCertVerify:$SkipCertVerification
+    $TargetNodeCertID = $RemoteCert[0]
+    $TargetNodeCertPath = $RemoteCert[1]
+    Write-Verbose "Get Cert from Node with Thumbprint:$($TargetNodeCertID) and exported to $($TargetNodeCertPath) ..."
+}
+catch {
+    Write-Host -fore red "Could not get cert from: $($Name)!`nException:`n$($_.Exception.Message)"
+    Continue
+}
+}
+#get GUID
+Write-Verbose "Trying to get GUID from AD for $($Name) ..."
+try {
+    $GUID = ([guid]([adsisearcher]"(samaccountname=$Name`$)").FindOne().Properties["objectguid"][0]).Guid
+}
+
+catch {
+    Write-Host -fore Yellow "No result from AD for: $($Name)!`nWill try with dNSHostName!"
+}
+
+If ($GUID -eq $Null) {
+    try {
+        #if NetBIOS fails try dNSHostname
+        $GUID = ([guid]([adsisearcher]"(dNSHostName=$Name)").FindOne().Properties["objectguid"][0]).Guid
+    }
+    catch {
+        Write-Host -fore Yellow "No result from AD for: $($Name)!`nWill skip this one!"
+        Continue
+    }
+}
+
+Write-Verbose "Found GUID:$($GUID) ..."
+Write-Verbose "Build node hashtable from CSV import ..."
+#create node hashtable
+[hashtable]$tempHash = $computer | ConvertTo-HashtableFromPsCustomObject
+$tempHash.Add("NodeName",$Name)
+
+#set certificate data
+If (!($SkipCert -eq $true)) {
+    $tempHash.Add("CertificateFile",$TargetNodeCertPath)
+    $tempHash.Add("Thumbprint",$TargetNodeCertID)
+}
+
+Write-Verbose "Add node hashtable for $($Name) to ConfigurationData ..."
+#add node hashtable to ConfigurationData
+$ConfigurationData.AllNodes = $ConfigurationData.AllNodes + $tempHash
+
+try {
+    Write-Verbose "Try to invoke command and build MOFs ..."
+    $VerbosePreferenceBefore = $VerbosePreference
+    $VerbosePreference = "SilentlyContinue"
+    $SB = {param($ConfigurationData) &$ConfigName -OutputPath $MOFspath -ConfigurationData $ConfigurationData}
+    Invoke-Command -ScriptBlock $SB -ArgumentList $ConfigurationData | Out-Null
+    $VerbosePreference = $VerbosePreferenceBefore
+    Write-Verbose "End of script ..."
+}
+catch {
+    $_.Exception #.Message
+}
+
+#publish MOFs to pull server
+If ($PublishToPull -eq $true) {
+    Write-Verbose "Publish $("$MOFsPath\$Name.mof") with $GUID to $MOFsTargetPath ..."
+    PublishMOF -MOFFilePath $("$MOFsPath\$Name.mof") -GUID $GUID -TargetFolder $MOFsTargetPath
+}
+}
+}
+
+End {
+}
